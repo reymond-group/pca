e6d728df1f6dba821df41b1f2067583d21441cab
Daniel Probst <daenuprobst@outlook.com>
2016-11-23 22:15:33 +0100

    Implemented color maps

--- create_maps.py
+++ create_maps.py
@@ -4,16 +4,26 @@ import sys
 import collections
 import linecache
 import argparse
+import colorsys
 import pandas as pd
 import numpy as np
 
+
+from sklearn import preprocessing
 from io import StringIO
+from colour import Color
 
 PARSER = argparse.ArgumentParser(
     description='Create RGB color maps from bins.')
 
-PARSER.add_argument('input', metavar='INPUT', type=str,
-                    help='the input (bins) file')
+PARSER.add_argument('means', metavar='MEANS', type=str,
+                    help='the means file')
+
+PARSER.add_argument('stds', metavar='STDS', type=str,
+                    help='the stds file')
+
+PARSER.add_argument('output', metavar='OUTPUT', type=str,
+                    help='the output (map) file')
 
 PARSER.add_argument('-d', '--delimiter', type=str, default=',',
                     help='use DELIMITER instead of \',\' for field delimiter')
@@ -21,13 +31,21 @@ PARSER.add_argument('-d', '--delimiter', type=str, default=',',
 PARSER.add_argument('-i', '--index', type=int, required=True,
                     help='the index for the column to be exported as an RGB color map')
 
-PARSER.add_argument('-c', '--chunksize', type=int, default=50000,
-                    help='the number of lines to be read from the INPUT file ' +
-                    'at a time and stored in memory, the default value is 50000')
-
 ARGS = PARSER.parse_args()
 
-READER = pd.read_csv(ARGS.input, sep=ARGS.delimiter, chunksize=ARGS.chunksize, header=None)
+ARGS.output = os.path.splitext(ARGS.output)[0]
 
-for chunk in READER:
-    print(chunk[0].describe())
+MEANS = pd.read_csv(ARGS.means, sep=ARGS.delimiter, usecols=[ARGS.index], header=None)
+STDS = pd.read_csv(ARGS.stds, sep=ARGS.delimiter, usecols=[ARGS.index], header=None)
+STDS = STDS.fillna(0)
+STDS = STDS.rename(columns = {0:1})
+
+MEANS = MEANS.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x))).values
+STDS = STDS.apply(lambda x: 1 - (0.5 * (x - np.min(x))) / (np.max(x) - np.min(x))).values
+
+#DATA = pd.concat([DATA, STDS], axis=1)
+
+with open(ARGS.output + '.map', 'a+') as f:
+    for i, row in enumerate(MEANS):
+        c = Color(hue=row[0], saturation=STDS[i][0], luminance=0.5)
+        f.write(str(round(c.red, 2)) + ',' + str(round(c.green, 2)) + ',' + str(round(c.blue, 2)) + '\n')
