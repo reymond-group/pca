1a7b17ac443ca2635c9201981bfdea541e61ca15
Daniel Probst <daenuprobst@gmail.com>
2017-01-08 20:13:51 +0100

    Added new indexing script

---
+++ index_file.py
@@ -0,0 +1,21 @@
+""" Index a file """
+import argparse
+
+PARSER = argparse.ArgumentParser(
+    description='Index a file with a from - to index for binary reading.')
+
+PARSER.add_argument('input', metavar='INPUT', type=str,
+                    help='the input (line separated) file')
+
+
+ARGS = PARSER.parse_args()
+
+start = 0
+
+with open(ARGS.input, 'r') as f:
+    with open(ARGS.input + '.index', 'a+') as g:
+        for line in f:
+            length = len(line)
+            g.write(str(start) + ',' + str(start + length) + '\n')
+            start += length
+
